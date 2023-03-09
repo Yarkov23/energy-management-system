9cca69ca455dd55d1265f536a4d0ee5092d57bf2
Yarkov23 <y.g.strukov@gmail.com>
2023-03-09 16:33:29 +0200

    Added entities for database

---
+++ src/main/java/com/example/energymanagement/entity/Company.java
@@ -0,0 +1,34 @@
+package com.example.energymanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+
+@Entity
+@Table(name = "companies")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hierarchy_level", referencedColumnName = "id")
+    private Company hierarchyLevel;
+
+    @OneToMany(mappedBy = "company")
+    private Set<Expense> expenses;
+
+    @OneToMany(mappedBy = "company")
+    private Set<Account> accounts;
+}
